3f2153bd26cfabe51463c9bf61f6b907e8aad325
diana <d1234@outlook.es>
2025-05-29 10:00:27 -0500

    hasta ejercicio 11

---
+++ consultas.sql
@@ -0,0 +1,8 @@
+#ejer 02
+select nombre, precio from producto;
+#ejer 03
+select * from producto ;
+#ejer 04
+select nombre, precio, round(precio*0.92, 2) as precio_eu from producto;
+
+
--- procedimientos.sql
+++ procedimientos.sql
@@ -1,13 +1,13 @@
 USE `entregabs`;
-DROP procedure IF EXISTS `insert_fabricantes`;
+DROP procedure IF EXISTS `insert_fabricante`;
 
 DELIMITER $$
 USE `entregabs`$$
-CREATE PROCEDURE `insert_fabricantes` (
+CREATE PROCEDURE `insert_fabricante` (
 in nom varchar (100)
 )
 BEGIN
-insert into fabricantes(nombre) values (nom);
+insert into fabricante(nombre) values (nom);
 END$$
 
 DELIMITER ;
@@ -23,8 +23,7 @@ in pre double,
 in codfab int
 )
 BEGIN
-insert into productos(nombre,precio,codigo_fabricante) values (nom,pre,codfab);
+insert into producto(nombre,precio,codigo_fabricante) values (nom,pre,codfab);
 END$$
 
 DELIMITER ;
-
